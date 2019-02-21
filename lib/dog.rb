30a77d2d1a86e2422e4c33a5cdafe36fc8d6f4fa
Ashley Bland <ashleyjbland@gmail.com>
2019-02-21 02:36:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,7 +49,7 @@ class Dog
     dog
   end
 
-  def self.find_by_id(id)
+  def self.find_by_id(num)
     sql = <<-SQL
       SELECT *
       FROM dogs
@@ -58,6 +58,19 @@ class Dog
     SQL
 
     DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
